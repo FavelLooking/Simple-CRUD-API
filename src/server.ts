7a1be93a5d737b9569772bafe00fb30239959dd2
FavelLooking <intwestpol@gmail.com>
2024-10-20 21:28:23 +0400

    feat: create and configure http server

--- src/server.ts
+++ src/server.ts
@@ -1,2 +1,17 @@
 import http from "http";
 import { lotrCharacters } from "./data/characters";
+
+const server = http.createServer((req, res) => {
+  if (req.method === "GET" && req.url === "/api/users") {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify(lotrCharacters));
+  } else {
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.end("Not Found");
+  }
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
