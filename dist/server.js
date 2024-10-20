a139c1e3bf408edefed25611b8330e794af91d60
FavelLooking <intwestpol@gmail.com>
2024-10-21 00:03:41 +0400

    feat: add get individual user feature

---
+++ dist/server.js
@@ -0,0 +1,41 @@
+import http from "http";
+import { validate as isValidUuid } from "uuid";
+import { lotrCharacters } from "./data/characters.js";
+const server = http.createServer((req, res) => {
+    const urlSplittedArray = req.url?.split("/").filter(Boolean);
+    console.log(urlSplittedArray);
+    if (req.method === "GET" &&
+        urlSplittedArray &&
+        urlSplittedArray[0] === "api" &&
+        urlSplittedArray[1] === "users" &&
+        urlSplittedArray.length === 2) {
+        res.writeHead(200, { "Content-Type": "application/json" });
+        res.end(JSON.stringify(lotrCharacters));
+    }
+    else if (req.method === "GET" &&
+        urlSplittedArray &&
+        urlSplittedArray[0] === "api" &&
+        urlSplittedArray[1] === "users" &&
+        urlSplittedArray.length === 3) {
+        console.log("hi!");
+        const userId = urlSplittedArray[2];
+        if (!isValidUuid(userId)) {
+            res.writeHead(400, { "Content-Type": "text/plain" });
+            res.end("Wrong UUID");
+            return;
+        }
+        const user = lotrCharacters.find((char) => char.id === userId);
+        res.writeHead(200, { "Content-Type": "application/json" });
+        res.end(JSON.stringify(user));
+    }
+    else {
+        res.writeHead(404, { "Content-Type": "text/plain" });
+        res.end("Not Found");
+    }
+    if (req.method === "GET" && req.url === `/api/users/user`) {
+    }
+});
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
--- src/data/characters.ts
+++ src/data/characters.ts
@@ -1,42 +1,44 @@
+import { v4 as uuidv4 } from "uuid";
+
 export const lotrCharacters = [
   {
-    id: "1e0b5d6a-7d50-4b1b-96d2-bbcd6aeabe9e",
+    id: uuidv4(),
     username: "Frodo Baggins",
     age: 33,
     hobbies: ["Cooking", "Gardening", "Adventuring"],
   },
   {
-    id: "5b2c5429-8db7-4d8b-8ed2-7c8329f26c1d",
+    id: uuidv4(),
     username: "Samwise Gamgee",
     age: 36,
     hobbies: ["Gardening", "Cooking", "Adventure"],
   },
   {
-    id: "b7e98a29-f96d-44f8-a23d-80c18c6078a2",
+    id: uuidv4(),
     username: "Gandalf the Grey",
     age: 2019,
     hobbies: ["Magic", "Traveling", "Wisdom"],
   },
   {
-    id: "c9c70a06-0e95-4c4d-a64f-c36bfa14e71c",
+    id: uuidv4(),
     username: "Legolas",
     age: 2931,
     hobbies: ["Archery", "Nature", "Adventure"],
   },
   {
-    id: "0b840e76-4f87-4e12-b007-41f28ab64b07",
+    id: uuidv4(),
     username: "Gimli",
     age: 139,
     hobbies: ["Axework", "Mining", "Battle"],
   },
   {
-    id: "18e80b45-4ecb-4c9f-b73f-cc1c9240e6db",
+    id: uuidv4(),
     username: "Aragorn",
     age: 87,
     hobbies: ["Swordsmanship", "Leadership", "Ranger"],
   },
   {
-    id: "a12d7984-df99-453c-8f4c-9d57763a74b7",
+    id: uuidv4(),
     username: "Boromir",
     age: 41,
     hobbies: ["Battle", "Honor", "Leadership"],
