ac2bb3d8b9eee107b207b8884da0aa2824546b33
FavelLooking <intwestpol@gmail.com>
2024-10-21 01:13:23 +0400

    feat: add post user feature

--- src/data/characters.ts
+++ src/data/characters.ts
@@ -1,46 +1,52 @@
 import { v4 as uuidv4 } from "uuid";
 
+export interface Character {
+  id: string;
+  username: string;
+  age: number;
+  hobbies: string[];
+}
 export const lotrCharacters = [
   {
-    id: uuidv4(),
     username: "Frodo Baggins",
     age: 33,
     hobbies: ["Cooking", "Gardening", "Adventuring"],
+    id: uuidv4(),
   },
   {
-    id: uuidv4(),
     username: "Samwise Gamgee",
     age: 36,
     hobbies: ["Gardening", "Cooking", "Adventure"],
+    id: uuidv4(),
   },
   {
-    id: uuidv4(),
     username: "Gandalf the Grey",
     age: 2019,
     hobbies: ["Magic", "Traveling", "Wisdom"],
+    id: uuidv4(),
   },
   {
-    id: uuidv4(),
     username: "Legolas",
     age: 2931,
     hobbies: ["Archery", "Nature", "Adventure"],
+    id: uuidv4(),
   },
   {
-    id: uuidv4(),
     username: "Gimli",
     age: 139,
     hobbies: ["Axework", "Mining", "Battle"],
+    id: uuidv4(),
   },
   {
-    id: uuidv4(),
     username: "Aragorn",
     age: 87,
     hobbies: ["Swordsmanship", "Leadership", "Ranger"],
+    id: uuidv4(),
   },
   {
-    id: uuidv4(),
     username: "Boromir",
     age: 41,
     hobbies: ["Battle", "Honor", "Leadership"],
+    id: uuidv4(),
   },
 ];
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import http from "http";
-import { validate as isValidUuid } from "uuid";
-import { lotrCharacters } from "./data/characters.js";
+import { validate as isValidUuid, v4 as uuidv4 } from "uuid";
+import { Character, lotrCharacters } from "./data/characters.js";
 import { sendResponse } from "./helpers/helpers.js";
 import dotenv from "dotenv";
 
@@ -9,35 +9,75 @@ dotenv.config();
 const server = http.createServer((req, res) => {
   const urlSplittedArray = req.url?.split("/").filter(Boolean);
 
-  if (
-    req.method === "GET" &&
-    urlSplittedArray &&
-    urlSplittedArray[0] === "api" &&
-    urlSplittedArray[1] === "users" &&
-    urlSplittedArray.length === 2
-  ) {
-    sendResponse(res, 200, "application/json", JSON.stringify(lotrCharacters));
-  } else if (
-    req.method === "GET" &&
-    urlSplittedArray &&
-    urlSplittedArray[0] === "api" &&
-    urlSplittedArray[1] === "users" &&
-    urlSplittedArray.length === 3
-  ) {
-    const userId = urlSplittedArray[2];
-    if (!isValidUuid(userId)) {
-      sendResponse(res, 400, "text/plain", "Wrong UUID");
-      return;
-    }
-    const user = lotrCharacters.find((char) => char.id === userId);
-    if (user) {
-      sendResponse(res, 200, "application/json", JSON.stringify(user));
-    } else {
-      sendResponse(res, 404, "text/plain", "User not found.");
-    }
-  } else {
-    res.writeHead(404, { "Content-Type": "text/plain" });
-    res.end("Not Found");
+  switch (req.method) {
+    case "GET":
+      if (
+        urlSplittedArray &&
+        urlSplittedArray[0] === "api" &&
+        urlSplittedArray[1] === "users"
+      ) {
+        if (urlSplittedArray.length === 2) {
+          sendResponse(
+            res,
+            200,
+            "application/json",
+            JSON.stringify(lotrCharacters),
+          );
+        } else if (urlSplittedArray.length === 3) {
+          const userId = urlSplittedArray[2];
+          if (!isValidUuid(userId)) {
+            sendResponse(res, 400, "text/plain", "Wrong UUID");
+            return;
+          }
+          const user = lotrCharacters.find((char) => char.id === userId);
+          if (user) {
+            sendResponse(res, 200, "application/json", JSON.stringify(user));
+          } else {
+            sendResponse(res, 404, "text/plain", "User not found.");
+          }
+        } else {
+          sendResponse(res, 404, "text/plain", "Not Found");
+        }
+      } else {
+        sendResponse(res, 404, "text/plain", "Not Found");
+      }
+      break;
+
+    case "POST":
+      if (
+        urlSplittedArray &&
+        urlSplittedArray[0] === "api" &&
+        urlSplittedArray[1] === "users"
+      ) {
+        let body = "";
+        req.on("data", (chunk) => (body += chunk.toString()));
+        req.on("end", () => {
+          try {
+            const newUser: Character = JSON.parse(body);
+
+            if (
+              !newUser.username ||
+              typeof newUser.username !== "string" ||
+              !newUser.age ||
+              typeof newUser.age !== "number" ||
+              !Array.isArray(newUser.hobbies) ||
+              !newUser.hobbies.every((hobby) => typeof hobby === "string")
+            ) {
+              sendResponse(res, 400, "text/plain", "Invalid user data");
+              return;
+            }
+
+            newUser.id = uuidv4();
+            lotrCharacters.push(newUser);
+            sendResponse(res, 201, "application/json", JSON.stringify(newUser));
+          } catch (error) {
+            sendResponse(res, 400, "text/plain", "Invalid JSON");
+          }
+        });
+      } else {
+        sendResponse(res, 404, "text/plain", "Not Found");
+      }
+      break;
   }
 });
 
